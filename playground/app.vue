6d548661647fea7d3b1c045e396c475ce2237e5b
Paul Cross <me@paulcross.cz>
2023-07-11 11:41:35 +0200

    update example

--- playground/app.vue
+++ playground/app.vue
@@ -3,6 +3,17 @@
 <template>
   <div>
     <h1>Nuxt permission</h1>
+    <ul>
+      <li>
+        <nuxt-link :to="{name: 'index'}">index</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{name: 'roles'}">roles</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{name: 'permissions'}">permissions</nuxt-link>
+      </li>
+    </ul>
     <NuxtPage />
   </div>
 </template>
